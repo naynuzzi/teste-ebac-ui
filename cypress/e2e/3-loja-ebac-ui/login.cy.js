89e12259669ee7a2a542dbde9372767003ff1a9f
naynuzzi <nayaranuzzi@yahoo.com.br>
2024-08-29 21:31:45 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac-ui/login.cy.js
+++ cypress/e2e/3-loja-ebac-ui/login.cy.js
@@ -1,24 +1,43 @@
 
 ///<reference types="cypress"/>
 
-// describe('example to-do app', () => {
-//    beforeEach(() => {
-//      // Cypress starts out with a blank slate for each test
-//      // so we must tell it to visit our website with the `cy.visit()` command.
-//      // Since we want to visit the same URL at the start of all our tests,
-//      // we include it in our beforeEach function so that it runs before each test
-//      cy.visit('https://example.cypress.io/todo')
-//    })
-// })
-
 describe('Funcionalidade: login', () => {
+   
+   beforeEach(() => {
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+   });
+   
+   afterEach(() => {
+      cy.screenshot()
+   
+   });
 
    it('Deve fazer login com sucesso', () => {
-      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+   
       cy.get('#username').type('naynuztest@teste.com.br')
       cy.get('#password').type('ayla1234')
       cy.get('.woocommerce-form > .button').click()
-
       cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, naynuztest (não é naynuztest? Sair)')
    })
+
+   it('Deve exibir uma mensagem de erro ao inserir o usuário inválido', () => {
+      
+      cy.get('#username').type('nnnnnppp@teste.com.br')
+      cy.get('#password').type('ayla1234')
+      cy.get('.woocommerce-form > .button').click()
+      cy.get('.woocommerce-error').should('exist')
+
+   });
+
+   it('Deve exibir um mensagem de erro ao inserir senha inválido', () => {
+      
+      cy.get('#username').type('naynuztest@teste.com.br')
+      cy.get('#password').type('xxxpp')
+      cy.get('.woocommerce-form > .button').click()
+
+      cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail naynuztest@teste.com.br está incorreta. Perdeu a senha?')
+      cy.get('.woocommerce-error').should('exist')
+      
+   });
+
 })
