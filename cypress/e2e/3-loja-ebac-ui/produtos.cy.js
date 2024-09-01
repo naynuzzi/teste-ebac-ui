aabfe337d76010e027d2fd06eb19d89218863091
naynuzzi <nayaranuzzi@yahoo.com.br>
2024-08-31 21:15:35 -0300

    pagina do produto

---
+++ cypress/e2e/3-loja-ebac-ui/produtos.cy.js
@@ -0,0 +1,18 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+   beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+   });
+   
+    it('Deve selecionar um produto da lista', () => {
+       cy.get('.product-block ')
+       //.first()
+       //.last()
+       //.eq(2)
+       .contains('Argus All-Weather Tank')
+       .click()
+
+       cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
+});
